b3559e388d08a560713772367a7b4dc3003756e0
rakuda-daraku <rakugaki.oukoku@gmail.com>
2017-10-31 09:22:43 +0900

    first commit

---
+++ todo-app/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <todo-list v-bind:todos="todos"/>
+    <create-todo v-on:add-todo="addTodo"/>
+  </div>
+</template>
+
+<script>
+  import TodoList from './components/TodoList'
+  import CreateTodo from './components/CreateTodo'
+
+  export default {
+    name: 'app',
+    components: {
+      TodoList,
+      CreateTodo
+    },
+    data() {
+      return {
+        todos:[
+          {
+            title: 'Todo A',
+            project: 'Project A',
+            done: false
+          },
+          {
+            title: 'Todo B',
+            project: 'Project B',
+            done: true
+          },
+          {
+            title: 'Todo C',
+            project: 'Project C',
+            done: false
+          },
+          {
+            title: 'Todo D',
+            project: 'Project D',
+            done: false
+          }
+        ]
+      }
+    },
+    methods: {
+      addTodo(title) {
+         this.todos.push({
+           title,
+           done: false
+         });
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ todo-app/src/components/TodoList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <todo v-on:delete-todo="deleteTodo" v-for="(todo, index) in todos" v-bind:todo="todo" :key="index"></todo>
+  </div>
+</template>
+
+<script type="text/javascript">
+  import Todo from "./Todo";
+
+  export default {
+    props:['todos'],
+    components: {
+      Todo,
+    },
+    methods: {
+      deleteTodo(todo) {
+        const todoIndex = this.todos.indexOf(todo);
+        this.todos.splice(todoIndex, 1);
+      }
+    }
+  };
+</script>
+<style>
+</style>
